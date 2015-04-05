653b205448b23df023296863daceac9b54b25556
Italo Soares <italocjs@live.com>
2015-04-04 21:34:52 -0300

    Update extension.js

--- extension.js
+++ extension.js
@@ -33,6 +33,33 @@
          }
 
          */
+         
+        bot.commands.allahuCommand = {
+            command: 'allahuakbar',  //The command to be called. With the standard command literal this would be: !bacon
+            rank: 'user', //Minimum user permission to use the command
+            type: 'exact', //Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
+            functionality: function (chat, cmd) {
+                if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                if (!bot.commands.executable(this.rank, chat)) return void (0);
+                else {
+                    API.sendChat("/me ALLAHU AKBAAAAR ALLAHU AKBAAAAAAAAAAAR!!!");
+                    API.sendChat("/me ALLAHU AKBAAAAR ALLAHU AKBAAAAAAAAAAAR!!!");
+                     var alahunm = Math.floor((Math.random() * 10) + 1);
+                    if (var > 8)
+                    {
+                    API.sendChat("/me SE FODEU, TOMOU BAN!!!");
+                    }
+                    if (var > 2 && var < 8)
+                    {
+                    API.sendChat("/me SE FODEU, FOI PRO ULTIMO!!!");
+                    }
+                    if (var > 0 && var <2)
+                    {
+                    API.sendChat("/me CAGAO, GANHOU FRIST!!!");
+                    }
+                }
+            }
+        };
 
         bot.commands.baconCommand = {
             command: 'bacon',  //The command to be called. With the standard command literal this would be: !bacon
